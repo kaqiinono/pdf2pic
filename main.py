744bc028eb130ba484ae12a0f9bf690c9680e918
songmeinuo <songmeinuo@jd.com>
2022-12-06 17:05:15 +0800

    backup

---
+++ main.py
@@ -0,0 +1,13 @@
+from utils.core import getalltxt, pyMuPDF_fitz
+
+if __name__ == "__main__":
+    # 1、PDF地址
+    pdfPath = './files'
+    # 2、需要储存图片的目录
+    imagePath = './imgs'
+    fileList = []
+    getalltxt(pdfPath, fileList)
+
+    for file in fileList:
+        fileName = file.split("/")[-1].split('.')[0]
+        pyMuPDF_fitz(file, imagePath + '/' + fileName)
